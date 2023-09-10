056c6ee7bf017f860bdcd2c1debe48981c120577
Divy Agrawal <divyagrawal29@gmail.com>
2023-09-10 23:14:40 +0530

    Fourth Change

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 import colors from "colors";
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.bgBlue.white);
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.error(`Error in Mongodb: ${error.message}`.bgRed.white);
+    process.exit(1);
   }
 };
 
